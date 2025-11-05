61158e349c3765407d9aa0edce927746c63b0592
zabao-qt <tagiabao.qt@gmail.com>
2025-11-05 08:34:58 +0700

    'init'

---
+++ Lab2_Timer/Lab2_Timer_1/Core/Src/software_timer.c
@@ -0,0 +1,127 @@
+/*
+ * software_timer.c
+ */
+
+/* Includes */
+#include "software_timer.h"
+#include "tim.h"
+
+#include "led_7seg.h"
+
+/* Variables */
+uint8_t timer2_flag = 0;
+uint16_t timer2_counter = 0;
+uint16_t timer2_mul = 0;
+
+uint8_t timer3_flag = 0;
+uint16_t timer3_counter = 0;
+uint16_t timer3_mul = 0;
+
+uint8_t timer4_flag = 0;
+//uint16_t timer4_counter = 0;
+//uint16_t timer4_mul = 0;
+uint32_t timer4_counter = 0;
+uint32_t timer4_mul = 0;
+
+/**
+ * @brief  	Init timer interrupt
+ * @param  	None
+ * @retval 	None
+ */
+void timer2_init(void) {
+	HAL_TIM_Base_Start_IT(&htim2);
+}
+
+void timer3_init(void) {
+	HAL_TIM_Base_Start_IT(&htim3);
+}
+
+void timer4_init(void) {
+	HAL_TIM_Base_Start_IT(&htim4);
+}
+
+/**
+ * @brief	Set duration of software timer interrupt
+ * @param	duration Duration of software timer interrupt
+ * @retval 	None
+ */
+void timer2_set(int ms) {
+	timer2_mul = ms / TIMER_CYCLE_2;
+	timer2_counter = timer2_mul;
+	timer2_flag = 0;
+}
+
+void timer3_set(int ms) {
+	timer3_mul = ms / TIMER_CYCLE_3;
+	timer3_counter = timer3_mul;
+	timer3_flag = 0;
+}
+
+//void timer4_set(int ms) {
+//	timer4_mul = ms / TIMER_CYCLE_4;
+//	timer4_counter = timer4_mul;
+//	timer4_flag = 0;
+//}
+
+void timer4_set_ms(int ms) {
+    /* us->ms then / tick */
+    uint32_t tick_us = TIMER_CYCLE_4_US;
+    if (tick_us == 0) tick_us = 1000; // safe guard (treat as 1ms)
+    uint64_t mul = ((uint64_t)ms * 1000ULL) / tick_us;
+    if (mul == 0) mul = 1; // minimum 1 tick
+    if (mul > 0xFFFFFFFFU) mul = 0xFFFFFFFFU;
+    timer4_mul = (uint32_t)mul;
+    timer4_counter = timer4_mul;
+    timer4_flag = 0;
+}
+
+void timer4_set_us(int us) {
+    uint32_t tick_us = TIMER_CYCLE_4_US;
+    if (tick_us == 0) tick_us = 1000;
+    uint64_t mul = (uint64_t)us / tick_us;
+    if (mul == 0) mul = 1;
+    if (mul > 0xFFFFFFFFU) mul = 0xFFFFFFFFU;
+    timer4_mul = (uint32_t)mul;
+    timer4_counter = timer4_mul;
+    timer4_flag = 0;
+}
+
+/**
+ * @brief  	Timer interrupt routine
+ * @param  	htim TIM Base handle
+ * @note	This callback function is called by default
+ * @retval 	None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	if (htim->Instance == TIM2) {
+		if (timer2_counter > 0) {
+			timer2_counter--;
+			if (timer2_counter == 0) {
+				timer2_flag = 1;
+				timer2_counter = timer2_mul;
+			}
+		}
+	}
+
+	if (htim->Instance == TIM3) {
+		if (timer3_counter > 0) {
+			timer3_counter--;
+			if (timer3_counter == 0) {
+				timer3_flag = 1;
+				timer3_counter = timer3_mul;
+			}
+		}
+	}
+
+	if (htim->Instance == TIM4) {
+		if (timer4_counter > 0) {
+			timer4_counter--;
+			if (timer4_counter == 0) {
+				timer4_flag = 1;
+				timer4_counter = timer4_mul;
+				led_7seg_display();
+			}
+		}
+	}
+}
+
